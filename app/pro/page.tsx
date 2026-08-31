ae5e05ca3396062b217dc46595ca59460c82a92d
Mark Scharbo <mscharbo@gmail.com>
2026-08-31 01:10:29 -0700

    Remove app directory - use pages router

--- app/pro/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProDashboard() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Pro Dashboard</h1>
-      <p>Historical data, daily alerts, conversion tracking</p>
-      <p>$199/month - Coming soon</p>
-    </div>
-  );
-}
